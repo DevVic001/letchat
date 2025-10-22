c08949199d464e4eba08db1dddc91f7a67bd3381
unknown <manuelmanuel1876u@gmail.com>
2025-10-22 23:49:26 +0100

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 
 // ✅ Connect to backend Socket.IO server
-const socket = io("http://localhost:5000", {
+const socket = io("https://letchatapp-rlir.onrender.com", {
   transports: ["websocket"], 
 });
 
